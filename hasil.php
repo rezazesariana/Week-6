db6fa5840762f0967bab525b4bade1f496b7af87
Reza Zesariana <rezazesariana58@gmail.com>
2024-03-30 17:15:41 +0700

    Belajar PHP

---
+++ hasil.php
@@ -0,0 +1,7 @@
+<?php
+$angka1 = $_POST["angkake1"];
+$angka2 = $_POST["angkake2"];
+
+$hasilkali = $angka1 * $angka2;
+echo "$angka1 x $angka2 = $hasilkali";
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+    echo "My first PHP script!<br>";
+    $nama = 'asep';
+    $usia = 76;
+    echo "Nama = $nama dengan usia $usia tahun";
+    $angkapertama = 25;
+    $angkakedua = 30;
+    $hasiltambah = $angkapertama + $angkakedua;
+    echo "<br>$angkapertama + $angkakedua = $hasiltambah";
+    ?>
+</body>
+</html>
